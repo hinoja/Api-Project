b824ec09ae92602b16a23daef981b26b97b7da98
hinoja <hinoja2@gmail.com>
2023-01-30 12:27:45 +0100

    Create  CRUD API User full

---
+++ app/Http/Controllers/api/Usercontroller.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class Usercontroller extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'min:8']
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            "Staus" => "1",
+            "user" => $user,
+            "message" => "User Created with successfull"
+        ]);
+    }
+    public function listUser()
+    {
+        $users = User::all();
+        return response()->json([
+            "Staus" => "1",
+            "user" => $users,
+            "message" => " all  Users of database"
+        ]);
+    }
+    public function delete($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        if ($user) {
+            return response()->json([
+                "Staus" => "1",
+                "user" => $user,
+                "message" => " This User data is deleted successfull"
+            ]);
+        } else {
+            return response()->json([
+                "Staus" => "0",
+                "message" => " Error,We don't find this User"
+            ]);
+        }
+    }
+    public function  login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8']
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token =  $user->createToken('auth_token')->plainTextToken;
+                // Auth::login($user);
+                return response()->json([
+                    "Staus" => "1",
+                    "info" => "Welcome " . $request->name,
+                    "access" => $token
+                ]);
+            } else {
+                return response()->json([
+                    "Staus" => "0",
+                    "info" => "Your accreditials are incorrect,retry"
+                ]);
+            }
+        } else {
+            return response()->json([
+                "Staus" => "0",
+                "info" => "This User don't exist in our database"
+            ]);
+        }
+    }
+
+    public function logout(){
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            "Staus" => "1",
+            "info" => "Deconnexion successfull"
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\api\Usercontroller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post("/register", [Usercontroller::class, 'register']);
+Route::post("/login", [Usercontroller::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get("/listUser", [Usercontroller::class, 'listUser']);
+    Route::delete("/destroy{id}", [Usercontroller::class,'delete']);
+    Route::get("/logout", [Usercontroller::class,'logout']);
+});
